7f57c9dbebdd524f1055d52ee53e8eb43376a4c3
nikuz <micurino@gmail.com>
2024-10-27 20:20:23 -0700

    add existing "color-scheme" meta tag removal, add :root "color-scheme" css property

--- content-scripts/content-script.ts
+++ content-scripts/content-script.ts
@@ -9,11 +9,19 @@ const host = window.location.host;
 })();
 
 function disableDarkMode() {
-    const metaNode = document.createElement('meta');
-    metaNode.name = 'color-scheme';
-    metaNode.content = 'light only';
+    const existingMetaTag = document.head.querySelector('meta[name="color-scheme"]');
+    if (existingMetaTag) {
+        document.head.removeChild(existingMetaTag);
+    }
+    
+    const newMetaTag = document.createElement('meta');
+    newMetaTag.name = 'color-scheme';
+    newMetaTag.content = 'only light';
+    document.head.appendChild(newMetaTag);
 
-    document.head.appendChild(metaNode);
+    const newStyleTag = document.createElement('style');
+    newStyleTag.appendChild(document.createTextNode(':root { color-scheme: only light; }'));
+    document.head.appendChild(newStyleTag);
 }
 
 chrome.storage.onChanged.addListener((changes) => {
